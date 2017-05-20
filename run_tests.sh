600dfe561a3e36d449f4f23639bbd8927eea8da0
Sean Johnson <Sean.Johnson@iag.com.au>
2017-05-20 23:17:46 +1000

    Docker Based Tests
    Issue #6
    This commit changes the Travic CI pipeline to run ansible in a docker container and additionally tests the deployment tasks.
    Updated README.md to contain new test script

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+apt-get update
+pushd /data
+ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass --syntax-check
+ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass
+ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass -v | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+
+
+
