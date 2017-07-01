517562dd02fd28b2468db5a5566c5392f03b7cb8
Ovv <contact@ovv.wtf>
2017-07-01 16:36:26 +0200

    travis ci

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 apt-get update
 pushd /data
-ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass --syntax-check
-ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass
-ansible-playbook -i environments/inventory test.yml --vault-password-file=.pass -v | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ansible-playbook -i environments/test_inventory deploy.yml -l travis --vault-password-file=.pass --syntax-check
+ansible-playbook -i environments/test_inventory deploy.yml -l travis --vault-password-file=.pass
+ansible-playbook -i environments/test_inventory deploy.yml -l travis --vault-password-file=.pass -v | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
